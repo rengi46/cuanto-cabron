622016edf834ffef56bbbc74610355ba151f249a
rogerpuig <rogerpuigdemasa@gmail.com>
2022-05-23 12:37:36 +0200

    seting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Meme } from "./components/card";
+import { CreaMeme } from "./components/fCreaMeme";
+import { NavBar } from "./components/navBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <NavBar/>
+      <div>
+      <CreaMeme/>
+      </div>
     </div>
   );
 }
